9b8aded6ae791397219b35c39611ea194e7041b8
King Heng Jonathan Lok <email": null>
2016-09-20 10:36:55 -0700

    add the methods code in the class

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.each {|drink| return drink if drink.brand == soda_brand}
+    nil
   end
 
   def sell(soda_brand)
+    @sodas.each do |drink|
+      if drink.brand == soda_brand
+        @cash += drink.price
+        return @sodas.delete(drink)
+      end
+    end
+    nil
   end
 
 end
