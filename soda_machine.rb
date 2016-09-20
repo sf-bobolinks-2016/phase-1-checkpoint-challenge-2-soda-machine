605920bb9e37d44a9b3ff0cb1e75fba358c19adc
Ty Daniels <ty.multisonus@gmail.com>
2016-09-20 10:08:32 -0700

    Soda machine enumerables passing rspec

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.select {|soda| return soda if soda.brand == soda_brand}
+    nil
   end
 
   def sell(soda_brand)
+    @sodas.select do |soda|  
+      if soda.brand == soda_brand
+        @cash += soda.price 
+        @sodas.delete(soda)
+        return soda
+      end
+    end
+    nil
   end
-
 end
