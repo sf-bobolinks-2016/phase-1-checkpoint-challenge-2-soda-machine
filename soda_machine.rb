b30776277b1b300abb6d19e5e00bd2b49acdd6de
Ali Baker <email": null>
2016-09-20 10:40:45 -0700

    Add code and comments to methods, pass rspec tests accordingly: - #current_inventory_count - #find_soda - #sell

--- soda_machine.rb
+++ soda_machine.rb
@@ -6,13 +6,28 @@ class SodaMachine
     @cash = args[:cash]
   end
 
+  # return the total number of sodas in the machine
   def current_inventory_count
+    @sodas.length
   end
 
+  # search through the Soda Machine's sodas array aka the inventory
+    # and pick the first soda that matches the soda_brand choice
   def find_soda(soda_brand)
+    @sodas.find {|soda| return soda if soda.brand == soda_brand}
   end
 
+  # will return nil if the soda brand selected is not found
+  # Otherwise, it will add the price of that selected soda to the
+    # Soda Machine's cash total then delete that particular instance of soda
+    # from the Soda Machine's sodas array aka the inventory
   def sell(soda_brand)
+    choice = find_soda(soda_brand)
+    return if choice == nil
+    @cash += choice.price
+    @sodas.delete choice
   end
 
 end
+
+# [#<Soda:0x007fb62402ef18 @brand="Coke Zero", @price=1.0>]
