10dfb90517e9dd1e6d3a8a417610615cff02e7f9
Jaime Rios <jaimejrios1@gmail.com>
2016-09-20 10:35:23 -0700

    make rspec tests pass

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,44 @@
 class SodaMachine
   attr_reader :sodas, :cash
 
+  #SodaMachine takes a hash as an argument
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
   end
 
+  #return the number of sodas in the soda machine
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      end
+    end
+    nil
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    if soda
+      add_cash
+      remove_soda(soda_brand)
+      return soda
+    end
+    nil
+  end
+
+  def add_cash
+    @cash += 1
+  end
+
+  def remove_soda(soda_brand)
+    sodas.delete_if do |soda|
+      soda.brand == soda_brand
+    end
   end
 
 end
