7c34290889d1296c382d960a2ddf60efa165bfde
Robbie Santos <robbiealixsantos@gmail.com>
2016-09-20 10:38:00 -0700

    Adds completed checkpoint challenge soda_machine.rb

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,21 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.detect {|soda| return soda if soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    @sodas.detect do |soda|
+       if soda.brand == soda_brand
+         @cash += soda.price
+         @sodas.delete(soda)
+         return soda
+       end
+     end
   end
 
 end
