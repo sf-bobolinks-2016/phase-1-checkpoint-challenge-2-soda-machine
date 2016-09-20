7d506199f3804354e28c2e25da8ddd4605ae44bf
Sam Leiken <email": null>
2016-09-20 10:07:12 -0700

    defines current_inventory_count, find_soda, and sell methods and passes all rspec tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,24 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      return soda if soda.brand == soda_brand
+    end
+    nil
   end
 
   def sell(soda_brand)
+    return nil if !find_soda(soda_brand)
+    if find_soda(soda_brand)
+      @cash += find_soda(soda_brand).price
+      soda = find_soda(soda_brand)
+      @sodas.delete(find_soda(soda_brand))
+      return soda
+    end
   end
 
 end
