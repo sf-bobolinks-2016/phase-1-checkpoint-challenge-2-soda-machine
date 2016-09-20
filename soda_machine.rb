a2e003bb63d050e53a464cd45ca604aec382f81f
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-09-20 11:23:59 -0700

    refactoring

--- soda_machine.rb
+++ soda_machine.rb
@@ -2,42 +2,40 @@ class SodaMachine
   attr_reader :sodas, :cash
 
   def initialize(args = {})
-    @sodas = args[:sodas]
-    @cash = args[:cash]
-    @inventory = []
-    @sodas.each do |soda|
-      @inventory << soda
-    end
+    @sodas = args[:sodas] #array of soda object
+    @cash = args[:cash] #total price
   end
 
 # A soda machine will be able to return the number of sodas it contains. This method should be called SodaMachine#current_inventory_count
   def current_inventory_count
-    @inventory.length
+    @sodas.length
   end
 
 # A soda machine will be able to find a single soda with a given brand. This method will be called SodaMachine#find_soda. 
 
   def find_soda(soda_brand)
-    # return nil if soda brand is not in machine
-    # filter through inventory and select soda_brand if in machine
-    return nil if @inventory.include?(soda_brand)
-    @inventory.select {|soda| soda == soda_brand }
-
+    #soda brand 'pepsi'
+    # find/soda
+    # array of soda of object
+    #loop through the array
+    #comparision
+      #soda.brand == brand_name
+      @sodas.find {|soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
-    # if soda_brand is in the soda machine
-    # return soda
-    # remove soda_brand from machine and add 1 to cash
-    # else return nil  
+   #find the soda with the given soda_brand
+   # 3 things: 
+   #1. add the price of soda to the toral cash
+   #2. delete the soda from the sodas collection
+   #3.return the soda
+    
+    # @soda = find_soda(soda_brand) 
+    # return bil if @soda.nil?
+    # @cash += @soda.price
+    # @sodas.delete(@soda)
 
-    if @inventory.include?(soda_brand)
-      @inventory.delete(soda_brand)
-      @cash += 1
-      return soda_brand
-    else
-      nil
-    end
+    @soda ? @cash += @soda.price && @sodas.delete(@soda) : nil
   end
 
 end
