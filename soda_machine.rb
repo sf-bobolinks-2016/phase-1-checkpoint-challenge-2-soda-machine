156de3d10fabd898ea860f9d55a489cb00739b85
Ty Daniels <ty.multisonus@gmail.com>
2016-09-20 10:20:55 -0700

    Pseudocoded solution w/ extra time

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,14 +7,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    #Call count method on sodas array to return number of soda totals.
     @sodas.count
   end
 
   def find_soda(soda_brand)
+    #Use enumerable to iterate over sodas looking for specific brand, returning nil by default.
     @sodas.detect {|soda| return soda if soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    #Iterate over sodas, and when brand is found, remove soda from machine (@sodas), and subtract price
+    #from overall cash (@cash).
     @sodas.detect do |soda|  
       if soda.brand == soda_brand
         @cash += soda.price 
