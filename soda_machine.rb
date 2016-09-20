aa6c3598c35b766e4bf9a31b959769ab069f181c
Ellie Dori <email": null>
2016-09-20 10:05:22 -0700

    Add code to soda machine class to make tests pass

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+   @sodas.find {|soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    desired = @sodas.find {|soda| soda.brand == soda_brand}
+    @cash += desired.price if desired
+    @sodas.delete(desired)
+    desired
   end
 
 end
