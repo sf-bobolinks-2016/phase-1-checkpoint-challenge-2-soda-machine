e6f6ebd4521572cbf8f4879d5de088c12187de58
Daniel Thompson <19dst89@gmail.com>
2016-09-20 10:39:27 -0700

    times up

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,19 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.find { |soda| soda.brand == soda_brand}
+    # @sodas.each { |soda| return soda if soda_brand == soda.brand }
   end
 
   def sell(soda_brand)
+    @sodas.each do |soda|
+      @cash += soda.price if soda.brand == soda_brand
+      soda.delete if soda_brand == soda.brand
+    end
   end
 
 end
